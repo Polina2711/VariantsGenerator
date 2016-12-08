cd66526b9955286cc082eb47a0b594ff4e3172c5
dbortsov <dbortsov@ATC16-12365>
2016-12-08 17:19:56 +0300

    Created DB 1.0

---
+++ StateExamVariants/StateExamVariants/Exercise.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateExamVariants
+{
+    public class Exercise
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ExNumber { get; set; }
+        [StringLength(100)]
+        [System.ComponentModel.DataAnnotations.Schema.Index(IsUnique = true)]
+        public string Level { get; set; }
+        public string Task { get; set; }
+        public string Solution { get; set; }
+        public string Answer { get; set; }
+
+        
+    }
+}
